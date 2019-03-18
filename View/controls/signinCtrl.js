1d80ab5aafc62d240603d9d032aef19f6ce7a4a8
Kazem Golabpars <hp.golab@gmail.com>
2019-03-18 15:07:26 -0500

    test

--- View/controls/signinCtrl.js
+++ View/controls/signinCtrl.js
@@ -1,8 +1,16 @@
 angular.module("yoFeul")
     .controller("signInCtrl", function ($scope) {
         $scope.user;
+        $scope.user;
 
         $scope.verifyUsername = function () {
+
+            var req = {
+                method: 'GET',
+                url: 'http://18.216.110.220:20000/function/login',
+                params: {username: $scope.user, password: $scope.pass}
+            }
+
             if ($scope.user) {
                 $scope.$parent.setUsername($scope.user);
                 window.location = '#!user/profile'
